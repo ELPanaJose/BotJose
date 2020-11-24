3b38499234681f0205e0e620a0dd71ca7f15ea78
ELPanaJose <69026987+ELPanaJose@users.noreply.github.com>
2020-11-23 19:51:19 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,63 +1,80 @@
 const linkMemes = [
-  "https://www.reddit.com/r/MAAU/",
-  "https://www.reddit.com/r/memes/",
-  "https://www.reddit.com/r/dankmemes/",
-  "https://www.reddit.com/r/Memes_Of_The_Dank",
-  "https://www.reddit.com/r/4chan"
+  "https://www.reddit.com/r/MAAU/new/",
+  "https://www.reddit.com/r/DylanteroYT/new/",
+  "https://www.reddit.com/r/MAAU/top/",
+  "https://www.reddit.com/r/DylanteroYT/hot/",
+  "https://www.reddit.com/r/MAAU/hot/",
+  "https://www.reddit.com/r/DylanteroYT/top/",
+  
 ];
-const {token}=require("./settings.json")
+
+const storyLinks = ["https://www.reddit.com/r/entitledparents/new/"];
+const historias = [];
 //packages
-const {Client} = require("discord.js"), // discord bots
+var img = [];
+const Discord = require("discord.js"), // discord bots
   axios = require("axios"), //http request
   cheerio = require("cheerio"); //web scrapping
-//links
-var img = [];
 //objects
-const commands = {
-    "/callao": function(msg) {
+
+const client = new Discord.Client(),
+  commands = {
+    "/callao": async function(msg) {
       msg.channel.send("when haces tus momos en video :V");
     },
-    "/newMomo": function(msg) {
+    "/nm": async function(msg) {
+      msg.channel.send("se busco un nuevo momo");
       linkMemes.map(i => {
-        axios
-          .get(i)
-          .then(r => {
-            let $ = cheerio.load(r.data);
-            $("._2_tDEnGMLxpM6uOa2kaDB3").each((i, e) => {
-              img.push($(e).attr("src"));
-            });
-          })
-          .catch(e => {
-            if (e) {
-              msg.channel.send(" no se ha encontrado un buen momo");
-            }
+        axios.get(i).then(r => {
+          let $ = cheerio.load(r.data);
+          $("._2_tDEnGMLxpM6uOa2kaDB3").each((i, e) => {
+            img.push($(e).attr("src"));
           });
+        });
       });
     },
+    "/momo": async function(msg) {
+      msg.channel.send(img[Math.floor(Math.random() * img.length)]).catch(e => {
+        msg.channel.send(
+          "mierda , no hemos encontrado un momo , seguramente bloquearon la ip :("
+        );
+      });
     },
-    "/momo": function(msg) {
-      msg.channel.send(img[Math.floor(Math.random() * img.length)]);
+    "/cantidadMomo": async function(msg) {
+      msg.channel.send(`la cantidad de momos es : ${img.length}`);
     },
-    "/monda": function(msg) {
+    "/monda": async function(msg) {
+      // no te va a funcionar si pones una lista asi , no deberias de hacer , por ahora solo pon una secuencia tipo     mmmh no  sabia
+      msg.channel.send("El pana brother jorge floyd todo un idolo");
+      msg.channel.send("https://www.youtube.com/watch?v=IRZWiqBHYaY");
       msg.channel.send(
-        "https://www.infobae.com/new-resizer/8iwerQ0t9dEThspv78qqDC0L7Uc=/768x432/filters:format(jpg):quality(85)/cloudfront-us-east-1.images.arcpublishing.com/infobae/GGPDEBMJVNDIDAOTSULULDIVDA.jpg"
+        "https://ichef.bbci.co.uk/news/640/cpsprodpb/8862/production/_112541943_whatsubject.jpg"
+      );
+
+      // salte y vuelve a meterte si quieres seguir probando, el servidor no se   quiere reiniciar    carajo
+    },
+    "/help": async function(msg) {
+      msg.channel.send(
+        "``` primero va /nm(nuevo momo) , luego  /momo y cada vez que quiers ver un nuevo momo es /nm, despues si quieres ver al pana brother jorge floyd pon /monda , y si quieres insultar pon /callao```"
       );
     }
   };
 
-Client.on("ready", () => {
-  Client.user.setPresence({
-    game: { name: "with discord.js" },
-    status: "idle"
+client.on("ready", () => {
+  client.user.setPresence({
+    game: {
+      name: "a",
+      type: "WATCHING"
+    },
+    status: "/help"
   });
 
-  console.log(`${Client.user.username} is up and running!`);
+  console.log(`${client.user.username} is up and running!`);
 
-  console.log(`BOT_JOSE LISTO!!! ${Client.user.tag}!`);
+  console.log(`BOT_JOSE LISTO!!! ${client.user.tag}!`);
 });
 
-
-Client.on("message", msg => {
+client.on("message", msg => {
   if (commands.hasOwnProperty(msg.content)) {
     commands[msg.content](msg);
   }
@@ -65,4 +82,4 @@ Client.on("message", msg => {
     msg.author.send("viva la grasa :V");
   }
 });
-Client.login(token);
+client.login("token");
