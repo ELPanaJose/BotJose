1a41d0ec1ff46f95fb82fd7c06c8b3265d2c4ae7
ELPanaJose <69026987+ELPanaJose@users.noreply.github.com>
2020-12-24 20:09:32 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,24 +5,45 @@ const linkMemes = [
   "https://www.reddit.com/r/DylanteroYT/hot/",
   "https://www.reddit.com/r/MAAU/hot/",
   "https://www.reddit.com/r/DylanteroYT/top/",
-  
+  "https://www.reddit.com/r/Colombia/hot/",
+  "https://www.reddit.com/r/Colombia/new/",
+  "https://www.reddit.com/r/Colombia/top/"
 ];
+
+Object.defineProperty(Array.prototype, "flat", {
+  value: function(depth = 1) {
+    return this.reduce(function(flat, toFlatten) {
+      return flat.concat(
+        Array.isArray(toFlatten) && depth > 1
+          ? toFlatten.flat(depth - 1)
+          : toFlatten
+      );
+    }, []);
+  }
+});
 
 const storyLinks = ["https://www.reddit.com/r/entitledparents/new/"];
 const historias = [];
 //packages
 var img = [];
-const Discord = require("discord.js"), // discord bots
+const Discord = require("discord.js"), // di
   axios = require("axios"), //http request
   cheerio = require("cheerio"); //web scrapping
+const help = new Discord.MessageEmbed()
+  .setTitle(
+    "puro momo rey 🤑" // 
+  )
+  .setDescription(
+    "``` *help te da informacion sobre los comandos ( for now the bot is only in spanih, but in a future it will be memes in english :D)\n\n *monda de solo puro momo \n\n  *bruh : si \n\n  *nm para agregar nuevos memes  \n\n *cm solo te da informacion de cuantos memes tienes \n\n *meme solo te da un momo \n\n *callao es solo para insultar ```"
+  );
 //objects
 
 const client = new Discord.Client(),
   commands = {
-    "/callao": async function(msg) {
-      msg.channel.send("when haces tus momos en video :V");
+    "*callao": async function(msg) {
+      msg.channel.send("Come monda echee <@720304994523283497>");
     },
-    "/nm": async function(msg) {
+    "*nm": async function(msg) {
       msg.channel.send("se busco un nuevo momo");
       linkMemes.map(i => {
         axios.get(i).then(r => {
@@ -33,30 +54,35 @@ const client = new Discord.Client(),
         });
       });
     },
-    "/momo": async function(msg) {
-      msg.channel.send(img[Math.floor(Math.random() * img.length)]).catch(e => {
-        msg.channel.send(
-          "mierda , no hemos encontrado un momo , seguramente bloquearon la ip :("
-        );
-      });
+    "*meme": async function(msg) {
+      let exampleEmbed = new Discord.MessageEmbed()
+        .setTitle(
+          "un momo :regional_indicator_b: :regional_indicator_r:  :regional_indicator_u: :regional_indicator_h:"
+        )
+        .setImage(img[Math.floor(Math.random() * img.length)]);
+      msg.channel.send(exampleEmbed);
     },
-    "/cantidadMomo": async function(msg) {
+    "*bruh": msg => {
+      msg.channel.send(":flushed:");
+      msg.channel.send("<:typescript:791822787689971772>")
+      msg.channel.send("https://media.giphy.com/media/VIOkcgpsnA2Zy/200.gif");
+    },
+    "*cm": async function(msg) {
       msg.channel.send(`la cantidad de momos es : ${img.length}`);
     },
-    "/monda": async function(msg) {
-      // no te va a funcionar si pones una lista asi , no deberias de hacer , por ahora solo pon una secuencia tipo     mmmh no  sabia
-      msg.channel.send("El pana brother jorge floyd todo un idolo");
+
+    "*monda": async function(msg) {
+ 
+      msg.channel.send("El pana brother jorge floyd todo un idolo  :bruh:");
       msg.channel.send("https://www.youtube.com/watch?v=IRZWiqBHYaY");
       msg.channel.send(
         "https://ichef.bbci.co.uk/news/640/cpsprodpb/8862/production/_112541943_whatsubject.jpg"
       );
 
-      // salte y vuelve a meterte si quieres seguir probando, el servidor no se   quiere reiniciar    carajo
+
     },
-    "/help": async function(msg) {
-      msg.channel.send(
-        "``` primero va /nm(nuevo momo) , luego  /momo y cada vez que quiers ver un nuevo momo es /nm, despues si quieres ver al pana brother jorge floyd pon /monda , y si quieres insultar pon /callao```"
-      );
+    "*help": async function(msg) {
+      msg.channel.send(help);
     }
   };
 
@@ -66,20 +92,17 @@ client.on("ready", () => {
       name: "a",
       type: "WATCHING"
     },
-    status: "/help"
+    status: "*help"
   });
-
-  console.log(`${client.user.username} is up and running!`);
 
   console.log(`BOT_JOSE LISTO!!! ${client.user.tag}!`);
 });
-
+/*
+aweonao nazi kkkkkk
+*/
 client.on("message", msg => {
   if (commands.hasOwnProperty(msg.content)) {
     commands[msg.content](msg);
-  }
-  if (/\:v/i.test(msg.content) && !msg.author.bot) {
-    msg.author.send("viva la grasa :V");
   }
 });
 client.login("token");
